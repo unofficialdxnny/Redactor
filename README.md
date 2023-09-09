35909e746c3d0999e422904644e5746fbf9ed4d0
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-09-09 01:46:57 +0100

    Redactor.py

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Redactor
 censor personal data such as faces in a photo or video
+
+----
+
+This programme uses OpenCV to process an image to blur out faces
+
+----
+
+## Image Input
+<p allign="center">
+<img src="img1.jpg">
+</p>
+
+## Image Output
+
+<p allign="center">
+<img src="blurred_image.jpg">
+</p>
+
+ps I think OpenCV thought the uncle on the bottom right was way to beutiful and couldnt disrespect him by blurring his face.
---
+++ redactor.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+image_src = input("Image Name> ")
+image_type = input("Type> ")
+
+image = cv2.imread(f"{image_src}.{image_type}")
+
+cascade_path = 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(cascade_path)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+for (x, y, w, h) in faces:
+    # Extract the face ROI (Region of Interest)
+    face_roi = image[y:y + h, x:x + w]
+
+    # Apply a Gaussian blur to the face ROI
+    blurred_face = cv2.GaussianBlur(face_roi, (99, 99), 30)
+
+    # Replace the original face region with the blurred face
+    image[y:y + h, x:x + w] = blurred_face
+
+
+cv2.imwrite('blurred_image.jpg', image)
